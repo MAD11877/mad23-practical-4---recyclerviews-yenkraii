1df8a51ed6a582f7c8420381c43f24d2517187dd
kiara <s10219186@connect.np.edu.sg>
2023-07-01 19:02:11 +0800

    changes if it ends with a 7

--- app/src/main/java/sg/edu/np/mad/madpractical/Adapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/Adapter.java
@@ -31,23 +31,28 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
         }
         return 1;
     }
-    public Adapter(ArrayList<User> obj){
-        this.ls = obj;
-        // stores the obj into the adaptor
-    }
-
-
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType==0){
-            View item2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_item, parent, false);
-            return new ViewHolder(item2);
-        }
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent) {
         View item1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_item, parent, false);
         return new ViewHolder(item1);
     }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item;
+        if (viewType == 1){
+             item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_item, parent, false);
+        }
+        else {
+             item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_item2, parent, false);
+        }
+        return new ViewHolder(item);
+
+    }
+
     public void onBindViewHolder(ViewHolder holder, int position){
         User s = ls.get(position);
+
         holder.name.setText(s.name);
         holder.descr.setText(s.description);
         holder.img.setOnClickListener(new View.OnClickListener() {
@@ -78,33 +83,6 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
                 alert.show();
             }
         });
-    }
-
-    private void AlertDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-        builder.setTitle("Profile");
-        builder.setMessage("MADness").setCancelable(true);
-        Integer i = 0;
-        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                AlertDialog();
-                //Integer i = randomOTP();
-                // go back to main activity
-                Intent intent = new Intent(mContext, MainActivity.class);
-                //intent.putExtra("value",i);
-                mContext.startActivity(intent);
-                dialog.cancel();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        AlertDialog alert = builder.create();
-        alert.show();
     }
 
     public int getItemCount(){
